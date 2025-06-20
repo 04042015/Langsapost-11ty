d5b0bc2c0ffa2e58bb77647fa72a819643e7d780
04042015 <m.iggopramulia14@gmail.com>
2025-06-21 02:10:52 +0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,10 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("assets");
-  eleventyConfig.addPassthroughCopy("style.css");
-  eleventyConfig.addCollection("artikel", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("artikel/*.md").sort((a, b) => b.date - a.date);
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addFilter("date", (dateObj, format = "dd LLL yyyy") => {
+    return DateTime.fromJSDate(dateObj).toFormat(format);
   });
-  eleventyConfig.addCollection("kategori", function(collectionApi) {
-    const map = {};
-    collectionApi.getFilteredByGlob("artikel/*.md").forEach(item => {
-      const cat = item.data.category;
-      if (!map[cat]) map[cat] = [];
-      map[cat].push(item);
-    });
-    return map;
-  });
+
   return {
     dir: {
       input: ".",
