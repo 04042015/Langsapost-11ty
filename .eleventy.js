4f2641e7fd5e65070c58d37f0d4efec503a591a0
04042015 <m.iggopramulia14@gmail.com>
2025-06-21 02:34:53 +0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
+  // Tambahkan filter tanggal
+  eleventyConfig.addFilter("date", (value, format = "dd LLLL yyyy") => {
+    return DateTime.fromJSDate(new Date(value), { zone: "utc" }).toFormat(format);
+  });
+
   return {
     dir: {
       input: ".",
